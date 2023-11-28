f2ee54ea4b58e68bf7c124c4afcfce529196e393
Jon Jackson <jonjacks@redhat.com>
2023-11-28 10:09:48 -0500

    Move constants out of main.go

--- cmd/bridge/main.go
+++ cmd/bridge/main.go
@@ -25,38 +25,6 @@ import (
 	"k8s.io/klog"
 )
 
-const (
-	k8sInClusterCA          = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-	k8sInClusterBearerToken = "/var/run/secrets/kubernetes.io/serviceaccount/token"
-
-	// Well-known location of the tenant aware Thanos service for OpenShift exposing the query and query_range endpoints. This is only accessible in-cluster.
-	// Thanos proxies requests to both cluster monitoring and user workload monitoring prometheus instances.
-	openshiftThanosTenancyHost = "thanos-querier.openshift-monitoring.svc:9092"
-
-	// Well-known location of the tenant aware Thanos service for OpenShift exposing the rules endpoint. This is only accessible in-cluster.
-	// Thanos proxies requests to the cluster monitoring and user workload monitoring prometheus instances as well as Thanos ruler instances.
-	openshiftThanosTenancyForRulesHost = "thanos-querier.openshift-monitoring.svc:9093"
-
-	// Well-known location of the Thanos service for OpenShift. This is only accessible in-cluster.
-	// This is used for non-tenant global query requests
-	// proxying to both cluster monitoring and user workload monitoring prometheus instances.
-	openshiftThanosHost = "thanos-querier.openshift-monitoring.svc:9091"
-
-	// Well-known location of Alert Manager service for OpenShift. This is only accessible in-cluster.
-	openshiftAlertManagerHost = "alertmanager-main.openshift-monitoring.svc:9094"
-
-	// Default location of the tenant aware Alert Manager service for OpenShift. This is only accessible in-cluster.
-	openshiftAlertManagerTenancyHost = "alertmanager-main.openshift-monitoring.svc:9092"
-
-	// Well-known location of the GitOps service. This is only accessible in-cluster
-	openshiftGitOpsHost = "cluster.openshift-gitops.svc:8080"
-
-	// Well-known location of the cluster proxy service. This is only accessible in-cluster
-	openshiftClusterProxyHost = "cluster-proxy-addon-user.multicluster-engine.svc:9092"
-
-	clusterManagementURL = "https://api.openshift.com/"
-)
-
 func main() {
 	fs := flag.NewFlagSet("bridge", flag.ExitOnError)
 	klog.InitFlags(fs)
